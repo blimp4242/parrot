ead0fd92b2b5a537a6f4e0bc10b6e6bec8660298
Simon Stjernholm <simon@stjernholm.dev>
2024-02-28 14:47:29 +0100

    update rspotify crate to v0.12.0 (#249)
    * updated rpsotify v0.11.6 -> v0.11.7
    
    * updated rpspotify to 0.12.0
    
    * fix clippy lints
    
    * fix cargo fmt check

--- src/commands/play.rs
+++ src/commands/play.rs
@@ -170,13 +170,9 @@ pub async fn play(
                     .ok_or(ParrotError::Other("failed to fetch playlist"))?;
 
                 for url in urls.iter() {
-<<<<<<< HEAD
                     let Ok(queue) =
                         enqueue_track(&call, &QueryType::VideoLink(url.to_string())).await
                     else {
-=======
-                    let Ok(queue) = enqueue_track(&call, &QueryType::VideoLink(url.to_string())).await else {
->>>>>>> a1409ae (Sync changes made to the main branch)
                         continue;
                     };
                     update_queue_messages(&ctx.http, &ctx.data, &queue, guild_id).await;
@@ -201,12 +197,8 @@ pub async fn play(
                     .ok_or(ParrotError::Other("failed to fetch playlist"))?;
 
                 for (idx, url) in urls.into_iter().enumerate() {
-<<<<<<< HEAD
                     let Ok(queue) = insert_track(&call, &QueryType::VideoLink(url), idx + 1).await
                     else {
-=======
-                    let Ok(queue) = insert_track(&call, &QueryType::VideoLink(url), idx + 1).await else {
->>>>>>> a1409ae (Sync changes made to the main branch)
                         continue;
                     };
                     update_queue_messages(&ctx.http, &ctx.data, &queue, guild_id).await;
@@ -239,13 +231,9 @@ pub async fn play(
                 let mut insert_idx = 1;
 
                 for (i, url) in urls.into_iter().enumerate() {
-<<<<<<< HEAD
                     let Ok(mut queue) =
                         insert_track(&call, &QueryType::VideoLink(url), insert_idx).await
                     else {
-=======
-                    let Ok(mut queue) = insert_track(&call, &QueryType::VideoLink(url), insert_idx).await else {
->>>>>>> a1409ae (Sync changes made to the main branch)
                         continue;
                     };
 
